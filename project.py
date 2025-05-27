ae9f83934bf7eb3ec3505a3256fb3b0a4e87b0b7
Dominik Podhorodecki <podhorodeckidominik@gmail.com>
2025-05-27 09:29:53 +0200

    Task9: Dodano asynchroniczne przetwarzanie

---
+++ project.py
@@ -0,0 +1,17 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+class Worker(QThread):
+ finished = pyqtSignal()
+ error = pyqtSignal(str)
+ def __init__(self, input_path, output_path):
+  super().__init__()
+  self.input_path = input_path
+  self.output_path = output_path def run(self):
+  try:
+   self.sleep(2)
+   self.finished.emit()
+  except Exception as e:   self.error.emit(str(e))
+class ConverterUI(QWidget):
+ def __init__(self):
+  super().__init__()
+  self.initUI()
+  self.worker = None
